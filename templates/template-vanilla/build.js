d7ef9d41e21044e29f2b781fa3b5986b8065cd63
TarsAndCase <eran.idar@gmail.com>
2024-05-19 20:41:17 +0200

    Added vanilla template

---
+++ templates/template-vanilla/build.js
@@ -0,0 +1,33 @@
+import { build, context } from "esbuild";
+import { copy } from "esbuild-plugin-copy";
+
+const mode = process.env.ENV;
+
+const config = {
+  entryPoints: ["src/main.js"],
+  platform: "node",
+  bundle: true,
+  sourcemap: mode == "dev",
+  outfile: "dist/main.js",
+  plugins: [
+    copy({
+      assets: {
+        from: ["./src/index.html", "./src/styles.css"],
+        to: ["."],
+      },
+    }),
+    copy({
+      assets: {
+        from: ["./src/assets/*"],
+        to: ["assets"],
+      },
+    }),
+  ],
+};
+
+if (mode == "watch") {
+  let ctx = await context(config);
+  await ctx.watch();
+} else {
+  await build(config);
+}
